5582bdb2d4ed7e04de9b81ad33c3eb9964fc4380
soufi <soufian2004ff@gmail.com>
2024-01-05 18:29:13 +0100

    printcomb2

--- PrintComb2.go
+++ PrintComb2.go
@@ -1,16 +1,18 @@
 package main
 import "fmt"
 func PrintComb2(){
-	for i:= 10 ; i<= 98 ; i++{
+	for i:= 00 ; i<= 98 ; i++{
 		for j := i+1 ; j <=99 ; j++ {
 	
-					fmt.Printf("%d %d, ",  i ,j)
+					fmt.Printf("%02d %02d, ",  i ,j)
+					if i < 98 || j < 99 {
+						fmt.Print()
 
 				}
 			}
 		}
 
-
+	}
 func main() {
 	PrintComb2()
 
